1743a0aed0f894b54b054bcda49fefd7aed64fa3
zcjl <zcjl516@gmail.com>
2012-02-23 13:21:02 +0800

    first version of octopress

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/zcjl/"
+http_images_path = "/zcjl/images"
+http_fonts_path = "/zcjl/fonts"
+css_dir = "public/zcjl/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
